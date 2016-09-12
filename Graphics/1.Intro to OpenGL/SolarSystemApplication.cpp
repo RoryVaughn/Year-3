7e17210ddeb5015cbf5324fe70ca34040c332088
Rory Vaughn <Rvaughn2525@gmail.com>
2016-09-12 16:31:36 -0500

    Almost have a plane drawn

--- Graphics/1.Intro to OpenGL/SolarSystemApplication.cpp
+++ Graphics/1.Intro to OpenGL/SolarSystemApplication.cpp
@@ -12,6 +12,143 @@ using glm::vec3;
 using glm::vec4;
 using glm::mat4;
 
+struct Vertex {
+	vec4 position;
+	vec4 color;
+};
+// our vertex and index buffers
+unsigned int m_VAO;
+unsigned int m_VBO;
+unsigned int m_IBO;unsigned int m_programID;
+int generateGrid()
+{
+	Vertex vertices[4];
+		unsigned int indices[4] = { 0,1,2,3 };
+
+		vertices[0].position = vec4(-5, 0, -5, 1);
+		vertices[1].position = vec4(5, 0, -5, 1);
+		vertices[2].position = vec4(-5, 0, 5, 1);
+		vertices[3].position = vec4(5, 0, 5, 1);
+
+		vertices[0].color = vec4(1, 0, 0, 1);
+		vertices[1].color = vec4(0, 1, 0, 1);
+		vertices[2].color = vec4(0, 0, 1, 1);
+		vertices[3].color = vec4(1, 1, 1, 1);
+
+	
+
+	// create and bind buffers to a vertex array object
+	glGenBuffers(1, &m_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex),
+	vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+	(void*)(sizeof(vec4)));
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+		glGenBuffers(1, &m_IBO);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 
+			sizeof(unsigned int), indices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+		// Generate our GL Buffers
+		// Lets move these so that they are all generated together
+		glGenBuffers(1, &m_VBO);
+		glGenBuffers(1, &m_IBO);
+		//Add the following line to generate a VertexArrayObject
+		glGenVertexArrays(1, &m_VAO);
+
+		glBindVertexArray(m_VAO);
+		// ....Code Segment here to bind and fill VBO + IBO
+		glBindVertexArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	return 0;
+}
+int CreateShader()
+{
+
+	// create shaders
+		const char* vsSource = "#version 410\n \
+	layout(location=0) in vec4 position; \
+	layout(location=1) in vec4 colour; \
+	out vec4 vColour; \
+	uniform mat4 projectionViewWorldMatrix; \
+	void main() { vColour = colour; gl_Position = projectionViewWorldMatrix * position;}";
+
+	const char* fsSource = "#version 410\n \
+	in vec4 vColour; \
+	out vec4 fragColor; \
+	void main() { fragColor = vColour; }";
+
+	int success = GL_FALSE;
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vertexShader, 1, (const char**)&vsSource, 0);
+	glCompileShader(vertexShader);
+	glShaderSource(fragmentShader, 1, (const char**)&fsSource, 0);
+	glCompileShader(fragmentShader);
+
+	m_programID = glCreateProgram();
+	glAttachShader(m_programID, vertexShader);
+	glAttachShader(m_programID, fragmentShader);
+	glLinkProgram(m_programID);
+
+	glGetProgramiv(m_programID, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE) {
+		int infoLogLength = 0;
+		glGetProgramiv(m_programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+		char* infoLog = new char[infoLogLength];
+
+		glGetProgramInfoLog(m_programID, infoLogLength, 0, infoLog);
+		printf("Error: Failed to link shader program!\n");
+		printf("%s\n", infoLog);
+		delete[] infoLog;
+	}
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+
+	// create shaders
+	vsSource = "#version 410\n \
+	layout(location=0) in vec4 position; \
+	layout(location=1) in vec4 colour; \
+	out vec4 vColour; \
+	uniform mat4 projectionViewWorldMatrix; \
+	void main() { vColour = Colour; gl_Position = projectionViewWorldMatrix * position;}";
+	fsSource = "#version 410\n \
+	in vec4 vColour; \
+	out vec4 fragColor; \
+	void main() { fragColor = vColour; }";
+	/*success = GL_FALSE;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vertexShader, 1, (const char**)&vsSource, 0);
+	glCompileShader(vertexShader);
+
+	glUseProgram(m_programID);
+	unsigned int projectionViewUniform =
+		glGetUniformLocation(m_programID, "projectionViewWorldMatrix");
+	glUniformMatrix4fv(projectionViewUniform, 1, false,
+		glm::value_ptr(m_projectionViewMatrix));
+
+	glBindVertexArray(m_VAO);
+	unsigned int indexCount = 6;
+	glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);*/
+
+	return 0;
+}
 SolarSystemApplication::SolarSystemApplication() 
 	: m_camera(nullptr),
 	m_direction(false) {
@@ -19,7 +156,7 @@ SolarSystemApplication::SolarSystemApplication()
 }
 
 SolarSystemApplication::~SolarSystemApplication() {
-
+	
 }
 
 bool SolarSystemApplication::startup() {
@@ -36,7 +173,7 @@ bool SolarSystemApplication::startup() {
 
 	// set input callback
 	setInputCallback(inputCallback);
-	
+	generateGrid();	CreateShader();
 	return true;
 }
 
@@ -66,47 +203,17 @@ bool SolarSystemApplication::update(float deltaTime) {
 	vec4 white(1, 1, 1, 0);
 	vec4 black(0, 0, 0, 1);
 
+
 	// for now let's add a grid to the gizmos
-	for (int i = 0; i < 21; ++i) {
-		Gizmos::addLine(vec3(-10 + i, 0, 10), vec3(-10 + i, 0, -10),
-			i == 10 ? white : black);
-		Gizmos::addLine(vec3(10, 0, -10 + i), vec3(-10, 0, -10 + i),
-			i == 10 ? white : black);
-	}
-
-	Gizmos::addTransform(mat4(1));
-
-	float time = (float)glfwGetTime();
-
-	// simple solar system
 	
-	// sun spins (reverses if direction says to)
-	mat4 sun = glm::rotate(time * 0.1f * (m_direction ? -1 : 1), vec3(0, 1, 0));
-
-	// earth spins around sun
-	mat4 earth = sun * glm::translate(vec3(8,0,0)) * glm::rotate(time, vec3(0,1,0));
-
-	// moon spins around earth but always facing the earth the same way
-	mat4 moon = earth * glm::rotate(time * 1.5f, vec3(0, 1, 0)) * glm::translate(vec3(1.5f, 0, 0));
 	
-	// add simple spheres for each
-	Gizmos::addSphere(vec3(sun[3]), 2, 16, 16, vec4(1, 1, 0, 1), &sun);
-	Gizmos::addSphere(vec3(earth[3]), 0.5f, 16, 16, vec4(0, 0, 1, 1), &earth);
-	Gizmos::addSphere(vec3(moon[3]), 0.125f, 16, 16, vec4(0.6f, 0.6f, 0.6f, 1), &moon);
 
-	// lines and transforms show how the matrices are relative
-	Gizmos::addLine(vec3(sun[3]), vec3(earth[3]), vec4(1, 0, 0, 1));
-	Gizmos::addLine(vec3(earth[3]), vec3(moon[3]), vec4(1, 0, 0, 1));
-	Gizmos::addTransform(sun, 3);
-	Gizmos::addTransform(earth, 1);
-	Gizmos::addTransform(moon, 0.5f);
-
+	
 	// return true, else the application closes
 	return true;
 }
 
 void SolarSystemApplication::draw() {
-
 	// clear the screen for this frame
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
--- Graphics/1.Intro to OpenGL/SolarSystemApplication.h
+++ Graphics/1.Intro to OpenGL/SolarSystemApplication.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include "BaseApplication.h"
+#include <glm/mat4x2.hpp>
+#include "gl_core_4_4.h"
 
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include "Camera.h"
+#include "Gizmos.h"
 // forward declared to reduce dependencies
 class Camera;
 
@@ -18,11 +26,11 @@ public:
 	virtual void draw();
 
 	static void inputCallback(GLFWwindow* window, int key, int scanline, int action, int mods);
-
+	glm::mat4 m_projectionViewMatrix;
 private:
 
 	Camera*		m_camera;
-
+	
 	// we can toggle the way the earth orbits the sun,
 	// demonstating input callback
 	bool		m_direction;
